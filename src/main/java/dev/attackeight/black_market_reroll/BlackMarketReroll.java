7fdf2453bdbe82db9cdd6389dbf4501a9a4f69db
attack8 <attackeight@gmail.com>
2024-10-29 14:48:42 -0400

    Begin work on config

--- src/main/java/dev/attackeight/black_market_reroll/BlackMarketReroll.java
+++ src/main/java/dev/attackeight/black_market_reroll/BlackMarketReroll.java
@@ -2,7 +2,9 @@ package dev.attackeight.black_market_reroll;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig.Type;
 import org.slf4j.Logger;
 
 @Mod(BlackMarketReroll.ID)
@@ -13,7 +15,9 @@ public class BlackMarketReroll {
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public BlackMarketReroll() {}
+    public BlackMarketReroll() {
+        ModLoadingContext.get().registerConfig(Type.SERVER, );
+    }
 
     public static ResourceLocation rl(String path) {
         return new ResourceLocation(BlackMarketReroll.ID, path);
--- src/main/java/dev/attackeight/black_market_reroll/ModConfig.java
+++ src/main/java/dev/attackeight/black_market_reroll/ModConfig.java
@@ -1,10 +1,18 @@
 package dev.attackeight.black_market_reroll;
 
 import net.minecraftforge.common.ForgeConfigSpec;
+import org.apache.commons.lang3.tuple.Pair;
 
 public class ModConfig {
 
     ModConfig(ForgeConfigSpec.Builder builder) {
-        final String item = "minecraft:diamond";
+        ForgeConfigSpec.ConfigValue<String> item =
+                builder.define("item", "minecraft:diamond");
+    }
+
+    static {
+        Pair<ModConfig, ForgeConfigSpec> pair = new ForgeConfigSpec.Builder()
+                .configure(ModConfig::new);
+        // Store pair values in some constant field
     }
 }
