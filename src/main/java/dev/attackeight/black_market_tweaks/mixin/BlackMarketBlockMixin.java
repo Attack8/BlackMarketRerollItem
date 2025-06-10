1d6993660a524ccbcfb92a2363533773348b8745
radimous <jurda92@gmail.com>
2025-06-11 00:21:30 +0200

    retain reroll item

--- src/main/java/dev/attackeight/black_market_tweaks/mixin/BlackMarketBlockMixin.java
+++ src/main/java/dev/attackeight/black_market_tweaks/mixin/BlackMarketBlockMixin.java
@@ -2,28 +2,34 @@ package dev.attackeight.black_market_tweaks.mixin;
 
 import dev.attackeight.black_market_tweaks.BlackMarketTweaks;
 import iskallia.vault.block.BlackMarketBlock;
-import iskallia.vault.block.entity.BlackMarketTileEntity;
+import iskallia.vault.block.base.InventoryRetainerBlock;
+import iskallia.vault.block.entity.base.InventoryRetainerTileEntity;
 import iskallia.vault.container.oversized.OverSizedInventory;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.Containers;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.List;
+
 @Mixin(value = BlackMarketBlock.class, remap = false)
-public class BlackMarketBlockMixin extends HorizontalDirectionalBlock {
+public abstract class BlackMarketBlockMixin extends HorizontalDirectionalBlock implements EntityBlock, InventoryRetainerBlock {
 
     protected BlackMarketBlockMixin(Properties p_54120_) {
         super(p_54120_);
@@ -36,27 +42,19 @@ public class BlackMarketBlockMixin extends HorizontalDirectionalBlock {
 
     @Override
     public void onRemove(BlockState state, @NotNull Level level, @NotNull BlockPos pos, BlockState newState, boolean isMoving) {
-        if (!state.is(newState.getBlock())) {
-            BlockEntity tile = level.getBlockEntity(pos);
-            if (tile instanceof BlackMarketTileEntity entity) {
-                try {
-                    OverSizedInventory c = (OverSizedInventory) entity.getClass().getDeclaredField("inventory").get(entity);
-                    c.getOverSizedContents().forEach((overSizedStack) ->
-                        overSizedStack.splitByStackSize().forEach((splitStack) ->
-                            Containers.dropItemStack(level, pos.getX(), pos.getY(), pos.getZ(), splitStack)
-                        )
-                    );
-                    c.clearContent();
-                } catch (Exception e) {
-                    BlackMarketTweaks.LOGGER.error(e.toString());
-                }
-                level.updateNeighbourForOutputSignal(pos, (Block) (Object) this);
-            }
-        }
-        if (state.hasBlockEntity() && (!state.is(newState.getBlock()) || !newState.hasBlockEntity())) {
-            level.removeBlockEntity(pos);
-        }
-
+        this.onInventoryBlockDestroy(level, pos);
+        super.onRemove(state, level, pos, newState, isMoving);
     }
 
+    @Override
+    public void appendHoverText(ItemStack stack, BlockGetter level, List<Component> tooltip, TooltipFlag flag) {
+        this.addInventoryTooltip(stack, tooltip, (is, tag) -> InventoryRetainerTileEntity.displayContentsOverSized(1, stacks -> {
+            stacks.addAll(OverSizedInventory.loadContents("inventory", (CompoundTag) tag));
+        }));
+    }
+
+    @Override
+    public void setPlacedBy(Level level, BlockPos pos, BlockState state, LivingEntity placer, ItemStack stack) {
+        this.onInventoryBlockPlace(level, pos, stack);
+    }
 }
--- src/main/java/dev/attackeight/black_market_tweaks/mixin/BlackMarketTileEntityMixin.java
+++ src/main/java/dev/attackeight/black_market_tweaks/mixin/BlackMarketTileEntityMixin.java
@@ -1,19 +1,19 @@
 package dev.attackeight.black_market_tweaks.mixin;
 
 import iskallia.vault.block.entity.BlackMarketTileEntity;
+import iskallia.vault.block.entity.base.InventoryRetainerTileEntity;
 import iskallia.vault.container.oversized.OverSizedInventory;
 import iskallia.vault.init.ModBlocks;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(value = BlackMarketTileEntity.class, remap = false)
-public class BlackMarketTileEntityMixin extends BlockEntity  {
+public class BlackMarketTileEntityMixin extends BlockEntity implements InventoryRetainerTileEntity {
 
     @Unique
     public final OverSizedInventory inventory = new OverSizedInventory(1, (BlackMarketTileEntity) (Object) this);
@@ -32,5 +32,17 @@ public class BlackMarketTileEntityMixin extends BlockEntity  {
     @Override
     protected void saveAdditional(@NotNull CompoundTag tag) {
         this.inventory.save(tag);
+    }
+
+    @Override public void storeInventoryContents(CompoundTag tag) {
+        this.inventory.save("inventory", tag);
+    }
+
+    @Override public void loadInventoryContents(CompoundTag tag) {
+        this.inventory.load("inventory", tag);
+    }
+
+    @Override public void clearInventoryContents() {
+        this.inventory.clearContent();
     }
 }
